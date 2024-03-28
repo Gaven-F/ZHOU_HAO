f706cb47fc838d2c8819c26067d495e8fd581439
fallen gaven <gavenf2023@outlook.com>
2024-03-28 15:24:31 +0800

    docs(client): 初始化

---
+++ ZHOU_HAO.Client/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Noto_Serif_SC } from "next/font/google";
+import "./globals.css";
+
+const noto = Noto_Serif_SC({ subsets: ["latin"], weight: "500" });
+
+export const metadata: Metadata = {
+	title: "Demo",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={noto.className}>{children}</body>
+		</html>
+	);
+}
